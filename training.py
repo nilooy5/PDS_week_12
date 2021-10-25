50c13f29fa2a3951e1db2f77c9563ac39602b10d
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-10-25 20:16:46 +0600

    grid search validation done from tutorial 12

--- training.py
+++ training.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from sklearn import datasets, neighbors, metrics, mixture, svm
 from sklearn.model_selection import train_test_split, GridSearchCV
 import matplotlib.pyplot as plt
@@ -24,16 +27,34 @@ gscv_classifier = GridSearchCV(
 
 gscv_classifier.fit(X_train, Y_train)
 
-print(gscv_classifier.best_params_)
+means = gscv_classifier.cv_results_['mean_test_score']
+stds = gscv_classifier.cv_results_['std_test_score']
+results = gscv_classifier.cv_results_['params']
 
-print(gscv_classifier.cv_results_['mean_test_score'])
-print(gscv_classifier.cv_results_['std_test_score'])
-print(gscv_classifier.cv_results_['params'])
+print("Grid scores on validation set:")
+print()
+
+for mean, std, param in zip(means, stds, results):
+    print("Parameter: %r, accuracy: %0.3f (+/-%0.03f)" % (param, mean, std*2))
+print()
+print("Best parameter:", gscv_classifier.best_params_)
 
 y_pred = gscv_classifier.predict(X_test)
 # • Plot confusion matrix and accuracy
 accuracy = metrics.accuracy_score(Y_test, y_pred) * 100
 plotcm = metrics.plot_confusion_matrix(gscv_classifier, X_test, Y_test, display_labels=class_names)
 plotcm.ax_.set_title('Accuracy = {0:.2f}%'.format(accuracy))
+
+
+def generate_image_from_plot():
+
+    file_name = "myplot.png"
+    file_dir = str(Path.home())
+    # completeName = os.path.join(file_dir, file_name)
+    complete_file_path = file_dir + '\\' + file_name
+    print("image file created at: " + file_dir + '\\' + file_name)
+    plt.savefig(complete_file_path, format='png')
+
+
+# generate_image_from_plot()
 plt.show()
-# plt.savefig('pic')
