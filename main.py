fe8ece31159865f19ba95103c888a45010e0cee4
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-10-25 16:00:25 +0600

    gui app for lab done

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ lbl_header.place(x=150, y=10)
 lbl = tk.Label(text="Select a colour: ", fg="navy", anchor="w", width=25, height=1, font=myFont)
 lbl.place(x=10, y=50)
 # Add variable var and 2 radio buttons
-var = tk.StringVar()
-rb1 = tk.Radiobutton(text="red", variable=var, value='r', font=myFont)
-rb1.place(x=180, y=50)
-rb1.deselect()
+var = tk.StringVar(value="red")
+# var.set(None)
 
-rb2 = tk.Radiobutton(text="blue", variable=var, value='b', font=myFont)
+rb1 = tk.Radiobutton(text="red", variable=var, value='red', font=myFont)
+rb1.place(x=180, y=50)
+# rb1.deselect()
+
+rb2 = tk.Radiobutton(text="blue", variable=var, value='blue', font=myFont)
 rb2.place(x=250, y=50)
-rb2.deselect()
+# rb2.deselect()
 # Label to display output when button is clicked
 lb_output = tk.Label(text="", fg="navy", anchor="w", width=25, height=1, font=myFont)
 lb_output.place(x=10, y=100)
@@ -31,9 +33,9 @@ lb_output.place(x=10, y=100)
 
 def select_item():
     selected = var.get()
-    if selected == 'r':
+    if selected == 'red':
         output = 'Red selected'
-    elif selected == 'b':
+    elif selected == 'blue':
         output = 'Blue selected'
     else:
         output = "Please select a colour"
@@ -43,7 +45,7 @@ def select_item():
 
 
 # Add button
-button = tk.Button(text="Run", fg="black", bg="lightblue", width=10, height=1, font=myFont, command=select_item)
+button = tk.Button(text="Run Model", fg="black", bg="lightblue", width=10, height=1, font=myFont, command=select_item)
 button.place(x=10, y=150)
 
 window.mainloop()
